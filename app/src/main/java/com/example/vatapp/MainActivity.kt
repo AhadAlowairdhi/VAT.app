6f6a0419b9b5c0888ae60a80cf3131b9f1e003c4
Ahad <odisha2226@gmail.com>
2022-08-08 12:23:42 +0300

    Added calculate VAT function

--- app/src/main/java/com/example/vatapp/MainActivity.kt
+++ app/src/main/java/com/example/vatapp/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.vatapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.vatapp.databinding.ActivityMainBinding
+import java.text.NumberFormat
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding : ActivityMainBinding
@@ -13,12 +14,37 @@ class MainActivity : AppCompatActivity() {
         setContentView(view) // link layout
 
         binding.btCalcVAT.setOnClickListener {
-            calculatVat()
+            calculateVat()
         } // End setOnClickListener
 
     } // End onCreate
 
-    private fun calculatVat() {
-        TODO("Not yet implemented")
-    }
+    private fun calculateVat() {
+        val stringVatet = binding.editTxt.text.toString()
+        val cost = stringVatet.toDouble()
+
+        // Radio group
+        val selectedID = binding.rgVat.checkedRadioButtonId
+
+        val vatPercentage = when(selectedID){
+            R.id.rbVat10 -> 0.10
+            R.id.rbVat15 -> 0.10
+            else -> 0.10
+        } // End when
+
+        var vat = vatPercentage * cost
+        var total = cost + vat
+        // Switch value
+        val roundVat = binding.switchRoundUp.isChecked
+
+        if (roundVat){
+            total = kotlin.math.ceil(total)
+        }
+
+        // Total Formatting
+        val formatedTotal = NumberFormat.getCurrencyInstance().format(total)
+
+        binding.txtCostTotal.text = getString(R.string.total_amount, formatedTotal)
+
+    } // End calculatVat() function
 } // End Class
